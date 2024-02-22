88af7d0298db092da0b9166ba3220305b70e35cb
Ian Dodds <ianmitchelldodds@gmail.com>
2024-02-22 12:14:35 -0500

    Add word game

---
+++ word-game.py
@@ -0,0 +1,37 @@
+name = input("Enter a name: ")
+place = input("Enter a place: ")
+animal1 = input("Enter an animal: ")
+name2 = input(f"Enter a name for the {animal1}: ")
+thing1 = input("Enter a thing: ")
+adjective1 = input("Enter an adjective: ")
+thing2 = input("Enter a thing: ")
+adjective2 = input("Enter an adjective: ")
+thing3 = input("Enter a thing: ")
+building = input("Enter a building: ")
+occupation = input("Enter an occupation: ")
+animal2 = input("Enter an animal: ")
+thing_plural = input("Enter a plural thing: ")
+adjective3 = input("Enter an adjective: ")
+
+story = f"""
+Once upon a time, in a far away land, there was a little
+boy named {name}. He lived with his mother in a {place}. 
+{name} and his mother were very poor and they
+had to sell their {animal1}, {name2}, to buy {thing1}. {name} was very
+{adjective1}. He loved {name2} very much. The next day, {name}'s
+mother sent him to the market to sell {name2}. On the way,
+{name} met a man who offered to buy {name2} for {thing2}. 
+{name} was very happy and he took the {thing2}. When
+{name}'s mother found out, she was very {adjective2} and she threw
+the {thing2} out of the window. The next morning, {name} and
+his mother woke up and saw a huge {thing3}. {name} was
+very excited and he climbed the {thing3}. At the top,
+{name} found a {building} and he went inside. There, he found
+a {occupation} and a {animal2} that laid {thing_plural}. {name} stole the
+{animal2} and ran down the {thing3}. The {occupation} followed him
+but {name} cut the {thing3} and the {occupation} fell and died.
+{name} and his mother were very {adjective3} and they lived
+happily ever after.
+"""
+
+print(story)
